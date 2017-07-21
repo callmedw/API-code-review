950ec5e1b0a7ddaf1a1c505d27d98a62da17cba4
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-07-21 11:07:24 -0700

    add creature and comment validation presence: true

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :creature
+  validates :author, :comment, presence: true
 end
--- app/models/creature.rb
+++ app/models/creature.rb
@@ -1,3 +1,4 @@
 class Creature < ApplicationRecord
   has_many :comments, dependent: :destroy
+  validates :name, :age, :gender, :coloring, :kind, presence: true
 end
