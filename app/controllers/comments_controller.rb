59109c2654e6914c7e01f099d51040e3696037c1
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-07-21 10:31:00 -0700

    comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,36 @@
+class CommentsController < ApplicationController
+  def index
+    @creature = Creature.find(params[:creature_id])
+    json_response(@creature.comments)
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+    json_response(@comment)
+  end
+
+  def create
+    @creature = Creature.find(params[:creature_id])
+    @comment = @creature.comments.create!(comment_params)
+    json_response(@comment.creature, :created)
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def comment_params
+    params.permit(:author, :comment, :creature_id)
+  end
+end
